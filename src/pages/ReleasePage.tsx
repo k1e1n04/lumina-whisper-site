196dcdad796014cdb7f584fe6e644fbf4164da75
KenIshii <riguveda44@gmail.com>
2026-04-06 23:05:24 +0900

    chore(release): publish 0.5.3 notes and appcast

--- src/pages/ReleasePage.tsx
+++ src/pages/ReleasePage.tsx
@@ -5,6 +5,13 @@ import PageSeo from '../components/PageSeo'
 
 const RELEASES = [
   {
+    version: '0.5.3',
+    date: '2026-04-06',
+    key: 'v053',
+    features: [] as string[],
+    fixes: ['llmEditorRoleEnforcement'],
+  },
+  {
     version: '0.5.2',
     date: '2026-04-06',
     key: 'v052',
