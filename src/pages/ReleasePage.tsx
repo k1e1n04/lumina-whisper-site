723795f3d2cc9b987bb2647ca38e83e08da09c34
KenIshii <riguveda44@gmail.com>
2026-04-06 23:26:19 +0900

    chore(release): publish 0.5.5 notes and appcast

--- src/pages/ReleasePage.tsx
+++ src/pages/ReleasePage.tsx
@@ -5,6 +5,13 @@ import PageSeo from '../components/PageSeo'
 
 const RELEASES = [
   {
+    version: '0.5.5',
+    date: '2026-04-06',
+    key: 'v055',
+    features: [] as string[],
+    fixes: ['llmOutputFallbackGuards'],
+  },
+  {
     version: '0.5.4',
     date: '2026-04-06',
     key: 'v054',
